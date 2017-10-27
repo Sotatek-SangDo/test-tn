04c72279aaf024327bb02b9e212d773541e35f61
Sotatek-SangDo <sang.do@sotatek.com>
2017-10-27 18:50:35 +0700

    add layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('script')
     <style type="text/css">
         .item {
-            width: 50%;
+            width: 100%;
             height: 60px;
             background: #f1f1f1;
             border-radius: 5px;
@@ -13,9 +13,16 @@
             width: 100%;
             color: #333;
         }
-        .material-icons {
+        .item .material-icons {
             padding-right: 30px;
             float: left;
+        }
+        .item {
+            background: #eaeaea;
+            box-shadow: 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12), 0 2px 4px -1px rgba(0,0,0,.2);
+        }
+        .mdl-grid mdl-cell {
+            background:#fefefe;
         }
         .mdl-list__item:after {
             content: '';
@@ -33,49 +40,53 @@
             font-size: 24px;
             font-weight: 600;
         }
+        .test {
+            padding-left: 10%;
+        }
+        .demo-list-item {
+            width: 100%;
+        }
     </style>
      <script type="text/javascript">
         window.localStorage.removeItem('start');
     </script>
 @endsection
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="test">
+<div class="mdl-grid mdl-cell mdl-cell--12-col mdl-cell--4-col-tablet mdl-card mdl-shadow--4dp">
+    <div class="test">
         <h1>Danh sách bài test</h1>
-            <ul class="demo-list-item mdl-list">
-                <li class="mdl-list__item item">
-                    <a href="{{ url('/test?mon=math') }}">
-                        <i class="material-icons">border_color</i>
-                        <span class="mdl-list__item-primary-content">Toán</span>
-                    </a>
-                </li>
-                <li class="mdl-list__item item">
-                    <a href="{{ url('/test?mon=math1') }}">
-                        <i class="material-icons">border_color</i>
-                        <span class="mdl-list__item-primary-content">Toán 1</span>
-                    </a>
-                </li>
-                <li class="mdl-list__item item">
-                    <a href="{{ url('/test?mon=ly') }}">
-                        <i class="material-icons">border_color</i>
-                        <span class="mdl-list__item-primary-content">Lý</span>
-                    </a>
-                </li>
-                <li class="mdl-list__item item">
-                    <a href="{{ url('/test?mon=hoa') }}">
-                        <i class="material-icons">border_color</i>
-                        <span class="mdl-list__item-primary-content">Hóa</span>
-                    </a>
-                </li>
-                <li class="mdl-list__item item">
-                    <a href="{{ url('/test?mon=anh') }}">
-                        <i class="material-icons">border_color</i>
-                        <span class="mdl-list__item-primary-content">Anh</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <ul class="demo-list-item mdl-list">
+            <li class="mdl-list__item item">
+                <a href="{{ url('/test?mon=math') }}">
+                    <i class="material-icons">border_color</i>
+                    <span class="mdl-list__item-primary-content">Toán</span>
+                </a>
+            </li>
+            <li class="mdl-list__item item">
+                <a href="{{ url('/test?mon=math1') }}">
+                    <i class="material-icons">border_color</i>
+                    <span class="mdl-list__item-primary-content">Toán 1</span>
+                </a>
+            </li>
+            <li class="mdl-list__item item">
+                <a href="{{ url('/test?mon=ly') }}">
+                    <i class="material-icons">border_color</i>
+                    <span class="mdl-list__item-primary-content">Lý</span>
+                </a>
+            </li>
+            <li class="mdl-list__item item">
+                <a href="{{ url('/test?mon=hoa') }}">
+                    <i class="material-icons">border_color</i>
+                    <span class="mdl-list__item-primary-content">Hóa</span>
+                </a>
+            </li>
+            <li class="mdl-list__item item">
+                <a href="{{ url('/test?mon=anh') }}">
+                    <i class="material-icons">border_color</i>
+                    <span class="mdl-list__item-primary-content">Anh</span>
+                </a>
+            </li>
+        </ul>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,93 +1,102 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
+    <meta name="description" content="thi trac nghiem online">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src = "https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+    <!-- <script src = "https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script> -->
+    <!-- <script src="https://code.getmdl.io/1.3.0/material.min.js"></script> -->
     <script src="{{ asset('js/getmdl-select.js') }}"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-pink.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
     @yield('script')
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Đăng nhập</a></li>
-                            <li><a href="{{ route('register') }}">Đăng kí</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('info') }}">Thông tin tài khoản</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('update-user') }}">Update tài khoản</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+    <div id="app" class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header class="mdl-layout__header mdl-layout__header--waterfall portfolio-header">
+            <div class="mdl-layout__header-row portfolio-logo-row">
+                <span class="mdl-layout__title">
+                    <div class="portfolio-logo"></div>
+                    <span class="mdl-layout__title">Thi trac nghiem online</span>
+                </span>
             </div>
-        </nav>
-
-        @yield('content')
+            <div class="mdl-layout__header-row portfolio-navigation-row mdl-layout--large-screen-only">
+                <nav class="mdl-navigation mdl-typography--body-1-force-preferred-font">
+                    <a class="mdl-navigation__link {{ Request::is('/') ? 'is-active' : '' }}" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+                    @guest
+                        <a class="mdl-navigation__link {{ Request::is('login') ? 'is-active' : '' }}" href="{{ route('login') }}">Đăng nhập</a>
+                        <a class="mdl-navigation__link {{ Request::is('register') ? 'is-active' : '' }}" href="{{ route('register') }}">Đăng kí</a>
+                    @else
+                        <a class="mdl-navigation__link {{ Request::is('user/info') ? 'is-active' : '' }}" href="{{ route('info') }}">Thông tin tài khoản</a>
+                        <a class="mdl-navigation__link {{ Request::is('user/update') ? 'is-active' : '' }}" href="{{ route('update-user') }}">Update tài khoản</a>
+                        <a class="mdl-navigation__link" href="#">Hi {{ Auth::user()->name }}
+                            <i class="glyphicon glyphicon-log-out" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"></i>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </a>
+                    @endguest
+                </nav>
+            </div>
+        </header>
+        <div class="mdl-layout__drawer mdl-layout--small-screen-only">
+            <nav class="mdl-navigation mdl-typography--body-1-force-preferred-font">
+                 <a class="mdl-navigation__link {{ Request::is('/') ? 'is-active' : '' }}" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+                    @guest
+                        <a class="mdl-navigation__link {{ Request::is('login') ? 'is-active' : '' }}" href="{{ route('login') }}">Đăng nhập</a>
+                        <a class="mdl-navigation__link {{ Request::is('register') ? 'is-active' : '' }}" href="{{ route('register') }}">Đăng kí</a>
+                    @else
+                        <a class="mdl-navigation__link {{ Request::is('user/info') ? 'is-active' : '' }}" href="{{ route('info') }}">Thông tin tài khoản</a>
+                        <a class="mdl-navigation__link {{ Request::is('user/update') ? 'is-active' : '' }}" href="{{ route('update-user') }}">Update tài khoản</a>
+                        <a class="mdl-navigation__link" href="#">Hi {{ Auth::user()->name }}
+                            <i class="glyphicon glyphicon-log-out" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"></i>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </a>
+                    @endguest
+            </nav>
+        </div>
+        <main class="">
+            <div class="container">
+                @yield('content')
+            </div>
+            <footer class="mdl-mini-footer">
+                <div class="mdl-mini-footer__left-section">
+                    <div class="mdl-logo">Product by SH team.</div>
+                </div>
+                <div class="mdl-mini-footer__right-section">
+                    <ul class="mdl-mini-footer__link-list">
+                        <li><a href="#">Giúp đỡ</a></li>
+                        <li><a href="#">Liên Hệ</a></li>
+                    </ul>
+                </div>
+            </footer>
+        </main>
     </div>
-
-    <!-- Scripts -->
+    <script type="text/javascript">
+        $(function() {
+            $('.mdl-navigation__link').hover(function() {
+                $(this).toggleClass('is-active');
+            })
+        })
+    </script>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
+
 </html>
