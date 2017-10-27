34bb8468b593fa0a06603f235b5e26a1f6bbc46f
Sotatek-SangDo <sang.do@sotatek.com>
2017-10-27 20:34:33 +0700

    mix css

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
+   .styles('public/css/styles.css', 'public/css/all.css')
    .version();
